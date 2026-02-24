0b0e5d87dded39d2f2b984e098a4ac553ab81231
Alfredo Aramburo <alfredo.aramburo420@gmail.com>
2026-02-24 17:19:07 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.CommandLine;
+
+namespace scl;
+
+class Program
+{
+    public static int taskIdCounter = 1;
+
+    static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand("TaskTracer Command-Line Tool");
+
+        //  Add Task
+        var addCommand = new Command("add", "Add a new task");
+        var updateCommand = new Command("update", "Update a task");
+        var deleteCommand = new Command("delete", "Delete a task");
+
+        rootCommand.Subcommands.Add(addCommand);
+        rootCommand.Subcommands.Add(updateCommand);
+        rootCommand.Subcommands.Add(deleteCommand);
+
+        addCommand.SetAction(parseResult =>
+        {
+            string task = Console.ReadLine() ?? string.Empty;
+            AddCommand(task);
+            return 0;
+        });
+
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
+    }
+
+    static void AddCommand(string task)
+    {
+        taskIdCounter += 1;
+        Console.WriteLine($"Output: Task added successfully(ID: {taskIdCounter})");
+    }
+}
