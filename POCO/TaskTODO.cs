a7aa044092124f513af380067e7250099b404072
Alfredo Aramburo <alfredo.aramburo420@gmail.com>
2026-02-25 11:19:41 -0600

    add: poco and json convertion

---
+++ POCO/TaskTODO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace scl.POCO
+{
+    internal class TaskTODO
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,55 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
+using System.Text.Json;
+using scl.POCO;
 
-int taskIdCounter = 1;
-
-//  Add a task
-var taskArgument = new Argument<string>("description")
+public class Program
 {
-    Description = "Task description"
-};
-var addCommand = new Command("add", "Add a new task")
-{
-    taskArgument
-};
+    static int taskIdCounter = 1;
+    public static int Main(string[] args)
+    {
+        // Add a task
+        var taskArgument = new Argument<string>("description")
+        {
+            Description = "Task description"
+        };
 
-addCommand.SetAction((parseResult) =>
-{
-    var description = parseResult.GetValue(taskArgument);
-    taskIdCounter += 1;
-    Console.WriteLine($"Output: Task added successfully(ID: {taskIdCounter})");
-    Console.WriteLine($"Task: {taskArgument}");
-    return 0;
-});
+        var addCommand = new Command("add", "Add a new task")
+        {
+            taskArgument
+        };
 
-var rootCommand = new RootCommand("TaskTracer Command-Line Tool");
-rootCommand.Subcommands.Add(addCommand);
+        addCommand.SetAction(parseResult =>
+        {
+            var description = parseResult.GetValue(taskArgument);
+            addTask(description);
+            return 0;
+        });
 
-return rootCommand.Parse(args).Invoke();
+        var rootCommand = new RootCommand("TaskTracer Command-Line Tool");
+        rootCommand.Subcommands.Add(addCommand);
+
+        return rootCommand.Parse(args).Invoke();
+    }
+
+    public static void addTask(string description)
+    {
+        TaskTODO newTask = new TaskTODO
+        {
+            id = taskIdCounter,
+            description = description,
+            status = "Pending",
+            createdAt = DateTime.Now,
+            updatedAt = DateTime.Now
+        };
+
+        taskIdCounter++;
+
+        string fileName = "TaskTODO.json";
+        string jsonString = JsonSerializer.Serialize(newTask);
+        File.WriteAllText(fileName, jsonString);
+
+        Console.WriteLine($"Output: Task added successfully (ID: {Guid.NewGuid()})");
+        Console.WriteLine($"Task: {newTask}");
+    }
+}
