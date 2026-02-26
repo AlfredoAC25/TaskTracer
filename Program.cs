c1829cdcdc4955c8451a9881595f167f470bc853
Alfredo Aramburo <alfredo.aramburo420@gmail.com>
2026-02-25 18:31:47 -0600

    add: upddate task by id

--- Program.cs
+++ Program.cs
@@ -7,28 +7,59 @@ using scl.Enums;
 public class Program
 {
     static int taskIdCounter = 1;
+    private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions { WriteIndented = true };
     public static int Main(string[] args)
     {
         // Add a task
-        var taskArgument = new Argument<string>("description")
+        var taskDescriptionArgument = new Argument<string>("description")
         {
             Description = "Task description"
         };
 
         var addCommand = new Command("add", "Add a new task")
         {
-            taskArgument
+            taskDescriptionArgument
         };
 
         addCommand.SetAction(parseResult =>
         {
-            var description = parseResult.GetValue(taskArgument);
-            addTask(description);
+            string? description = parseResult.GetValue(taskDescriptionArgument);
+            if (!string.IsNullOrWhiteSpace(description)) addTask(description);
+
+            return 0;
+        });
+
+        // Update a task
+        var updateTaskArgument = new Argument<string>("description")
+        {
+            Description = "Task description to update"
+        };
+
+        var getTaskIdArgument = new Argument<int>("id")
+        {
+            Description = "Task ID"
+        };
+
+        var updateCommand = new Command("update", "Update a new task")
+        {
+            getTaskIdArgument,
+            updateTaskArgument
+        };
+
+        updateCommand.SetAction(parseResult =>
+        {
+            string? description = parseResult.GetValue(updateTaskArgument);
+            int idInput = parseResult.GetValue(getTaskIdArgument);
+
+            if (!string.IsNullOrWhiteSpace(description)) updateTask(idInput, description);
+            else Console.WriteLine("Invalid input for ID or description.");
+                
             return 0;
         });
 
         var rootCommand = new RootCommand("TaskTracer Command-Line Tool");
         rootCommand.Subcommands.Add(addCommand);
+        rootCommand.Subcommands.Add(updateCommand);
 
         return rootCommand.Parse(args).Invoke();
     }
@@ -44,7 +75,6 @@ public class Program
             tasks = JsonSerializer.Deserialize<List<TaskTODO>>(existingJson) ?? new List<TaskTODO>();
         }
         
-
         int taskIdCounter = tasks.Count > 0 ? tasks.Max(tasks => tasks.id) + 1 : 1;
 
         TaskTODO newTask = new TaskTODO
@@ -59,11 +89,32 @@ public class Program
         tasks.Add(newTask);
 
         string fileName = "TaskTODO.json";
-        string jsonString  = JsonSerializer.Serialize<List<TaskTODO>>(tasks, new JsonSerializerOptions { WriteIndented = true });
+        string jsonString  = JsonSerializer.Serialize<List<TaskTODO>>(tasks, JsonOptions);
 
         File.WriteAllText(fileName, jsonString);
 
         Console.WriteLine($"Output: Task added successfully (ID: {Guid.NewGuid()})");
         Console.WriteLine($"Task: {newTask}");
+    }
+
+    public static void updateTask(int id, string description)
+    {
+        List<TaskTODO> tasks = new List<TaskTODO>();
+        string fileName = "TaskTODO.json";
+
+        string existingJson = File.ReadAllText("TaskTODO.json");
+
+        if (!string.IsNullOrWhiteSpace(existingJson))
+        {
+            tasks = JsonSerializer.Deserialize<List<TaskTODO>>(existingJson) ?? new List<TaskTODO>();
+        }
+
+        TaskTODO? task = tasks.FirstOrDefault(t => t.id == id);
+
+        task?.description = description;
+
+        string jsonString = JsonSerializer.Serialize<List<TaskTODO>>(tasks, JsonOptions);
+
+        File.WriteAllText(fileName, jsonString);            
     }
 }
